2a14a1752cf39b73b46b1b6f91a491ccc5566c35
Alan Nguyen <tuan8101@gmail.com>
2020-03-16 23:33:02 -0600

    Create fetch weather from API

--- weather.js
+++ weather.js
@@ -1,7 +1,17 @@
 class Weather {
   constructor(city, state) {
-    this.apikey = '99dfe35fcb7de1ee';
+    this.apiKey = '99dfe35fcb7de1ee';
     this.city = city;
     this.state = state;
+  }
+  // Fetch weather from API
+  async getWeather() {
+    const response = await fetch(
+      `http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`
+    );
+
+    const responseData = await response.json();
+
+    return responseData.current_observation;
   }
 }
