120127cddc3451b6264662647c84903c6f653901
Alan Nguyen <tuan8101@gmail.com>
2020-03-17 22:40:56 -0600

    Get weather on DOM load

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init weather object
 const weather = new Weather('Boston', 'MA');
 
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
 // weather.changeLocation('Miami', 'FL');
 
 function getWeather() {
