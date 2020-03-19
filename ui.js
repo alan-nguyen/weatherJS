251bac870aae90d56068f13e5a9bd8909b3702d4
Alan Nguyen <tuan8101@gmail.com>
2020-03-18 21:30:04 -0600

    Create paint function

--- ui.js
+++ ui.js
@@ -10,4 +10,15 @@ class UI {
     this.dewpoint = document.getElementById('w-dewpoint');
     this.wind = document.getElementById('w-wind');
   }
+
+  paint(weather) {
+    this.location.textContent = weather.display_locatiion.full;
+    this.desc.textContent = weather.weather;
+    this.string.textContent = weather.temperature_string;
+    this.icon.setAttribute('src', weather.icon_url);
+    this.humidity.textContent = `Relative Humidity: ${weather.relative_humidity}`;
+    this.feelsLike.textContent = `Feels Like: ${weather.feelsLike_string}`;
+    this.dewpoint.textContent = `DewPoint: ${weather.dewpoint_string}`;
+    this.wind.textContent = `Wind: ${weather.wind_string}`;
+  }
 }
