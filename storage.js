3ba515cc6dfe5b4e8cba44317013eb08a6799153
Alan Nguyen <tuan8101@gmail.com>
2020-03-18 21:44:37 -0600

    Create getLocationData method

--- storage.js
+++ storage.js
@@ -6,7 +6,24 @@ class Storage {
     this.defaultState = 'FL';
   }
 
-  getLocationData() {}
+  getLocationData() {
+    if (localStorage.getItem('city') === null) {
+      this.city = this.defaultCity;
+    } else {
+      this.city = localStorage.getItem('city');
+    }
+
+    if (localStorage.getItem('state') === null) {
+      this.state = this.defaultState;
+    } else {
+      this.state = localStorage.getItem('state');
+    }
+
+    return {
+      city: this.city,
+      state: this.state
+    };
+  }
 
   setLocationData(city, state) {
     localStorage.setItem('city', city);
