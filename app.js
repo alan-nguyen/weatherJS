f23eff8dcb6ac532478b8bc1e3d2ce4040914bd5
Alan Nguyen <tuan8101@gmail.com>
2020-03-18 21:38:36 -0600

    Create chagne location event

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const ui = new UI();
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// weather.changeLocation('Miami', 'FL');
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', e => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  weather.changeLocation('Miami', 'FL');
+});
 
 function getWeather() {
   weather
